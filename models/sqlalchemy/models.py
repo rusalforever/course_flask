756cd6969806e56d07602af9fb3d30b45d2d87a3
oleh_donchenko <od.don.it@gmail.com>
2024-04-13 21:42:23 +0300

    Homework 3 - "Animal Shelter" project

--- models/sqlalchemy/models.py
+++ models/sqlalchemy/models.py
@@ -9,3 +9,6 @@ class Animal(db.Model):
     animal_type = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
+    breed = db.Column(db.String, nullable=False)
+    photo_url = db.Column(db.String, nullable=True)
+    animal_age = db.Column(db.Integer, nullable=False)
