4d71aba8eb8a07df245c8f416746da238e81bb50
hwork_1_volodymyr_bakalec <jumakloxa@gmail.com>
2024-05-10 20:17:10 +0300

    Merge remote-tracking branch 'origin/volodymyr_bakalec_hw' into volodymyr_bakalec_hw

--- models/sqlalchemy/models.py
+++ models/sqlalchemy/models.py
@@ -7,5 +7,7 @@ class Animal(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     animal_type = db.Column(db.String, nullable=False)
+    animal_breed = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
+    photo_url = db.Column(db.String, nullable=False)
